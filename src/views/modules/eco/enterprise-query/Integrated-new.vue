1fe32acea2189ab241e54d7b048d72b720a79353
汪浩 <471761282@qq.com>
2020-08-14 16:38:17 +0800

    提交

---
+++ src/views/modules/eco/enterprise-query/Integrated-new.vue
@@ -0,0 +1,13 @@
+<template>
+<div>测试2</div>
+</template>
+
+<script>
+  export default {
+    name: 'Integrated-new'
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/modules/eco/enterprise-query/panoramic-inf.vue
+++ src/views/modules/eco/enterprise-query/panoramic-inf.vue
@@ -1,204 +1,130 @@
-<!--操作日志明细-->
 <template>
-  <div class="mod-config">
-    <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
-      <el-form-item>
-        <el-input v-model="dataForm.key" placeholder="填写名称搜索" size="small" clearable></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button @click="getDataList()" size="small">查询</el-button>
-      </el-form-item>
-    </el-form>
-    <el-table size="small"
-              :data="dataList"
-              border
-              v-loading="dataListLoading"
-              @selection-change="selectionChangeHandle"
-              style="width: 100%;">
-      <el-table-column
-        type="selection"
-        header-align="center"
-        align="center"
-        width="50">
-      </el-table-column>
-      <el-table-column
-        prop="sCode"
-        header-align="left"
-        align="left"
-        show-overflow-tooltip
-        label="流水号">
-      </el-table-column>
-      <el-table-column
-        prop="tabName"
-        header-align="left"
-        align="left"
-        show-overflow-tooltip
-        label="操作表名">
-      </el-table-column>
-      <el-table-column
-        prop="method"
-        header-align="left"
-        align="left"
-        show-overflow-tooltip
-        label="调用方法">
-      </el-table-column>
-      <el-table-column
-        prop="operatorType"
-        header-align="left"
-        align="left"
-        width="80"
-        label="操作类型">
-        <template slot-scope="scope">
-          <el-tag v-if="scope.row.operatorType == '0'" size="small">其他</el-tag>
-          <el-tag v-if="scope.row.operatorType == '1'" size="small">新增</el-tag>
-          <el-tag v-if="scope.row.operatorType == '2'" size="small">删除</el-tag>
-          <el-tag v-if="scope.row.operatorType == '3'" size="small">修改</el-tag>
-          <el-tag v-if="scope.row.operatorType == '4'" size="small">查询</el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column
-        prop="operatorSql"
-        header-align="left"
-        align="left"
-        show-overflow-tooltip
-        label="操作SQL">
-      </el-table-column>
-      <el-table-column
-        prop="beforeData"
-        header-align="left"
-        align="left"
-        show-overflow-tooltip
-        label="操作前数据">
-      </el-table-column>
-      <el-table-column
-        prop="afterData"
-        header-align="left"
-        align="left"
-        show-overflow-tooltip
-        label="操作后数据">
-      </el-table-column>
-      <el-table-column
-        prop="operatorCond"
-        header-align="left"
-        align="left"
-        show-overflow-tooltip
-        label="SQL条件">
-      </el-table-column>
-      <el-table-column
-        prop="operParam"
-        header-align="left"
-        align="left"
-        show-overflow-tooltip
-        label="操作参数">
-      </el-table-column>
-      <el-table-column
-        prop="status"
-        header-align="left"
-        align="left"
-        width="80"
-        label="执行状态">
-        <template slot-scope="scope">
-          <el-tag v-if="scope.row.status == '0'" size="small">成功</el-tag>
-          <el-tag v-if="scope.row.status == '1'" size="small" type="danger">失败</el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column
-        prop="errorMsg"
-        header-align="left"
-        align="left"
-        show-overflow-tooltip
-        label="错误信息">
-      </el-table-column>
-      <el-table-column
-        prop="operTime"
-        header-align="left"
-        align="left"
-        show-overflow-tooltip
-        label="操作时间">
-      </el-table-column>
-      <el-table-column
-        prop="jsonResult"
-        header-align="left"
-        align="left"
-        show-overflow-tooltip
-        label="执行结果">
-      </el-table-column>
-    </el-table>
-    <el-pagination
-      @size-change="sizeChangeHandle"
-      @current-change="currentChangeHandle"
-      :current-page="pageIndex"
-      :page-sizes="[10, 20, 50, 100]"
-      :page-size="pageSize"
-      :total="totalPage"
-      layout="total, sizes, prev, pager, next, jumper">
-    </el-pagination>
+  <div class="main-container">
+    <el-row :gutter="20">
+      <el-col :span="3" :xs="24">
+        <el-tabs :tab-position="tabPosition" v-model="currentView">
+          <el-tab-pane
+            :label="item.name"
+            :name="item.path"
+            v-for="(item,index) in menu"
+            :key="index"
+          ></el-tab-pane>
+        </el-tabs>
+      </el-col>
+      <el-col :span="21" :xs="24">
+        <div class="head">
+          <div class="head-box">
+            <div class="head-left">小米科技有限责任公司</div>
+            <div class="head-right">
+              <dl>
+                <dd>健康描述</dd>
+                <dt>
+                  <p>619</p><span>比较健康</span>
+                </dt>
+              </dl>
+              <dl>
+                <dd>收入规模</dd>
+                <dt>
+                  <p>500亿-700亿</p>
+                </dt>
+              </dl>
+              <dl>
+                <dd>行业领域</dd>
+                <dt>
+                  <p>科学与技术研究</p>
+                </dt>
+              </dl>
+            </div>
+          </div>
+        </div>
+        <!--:is 的作用：会将div标签转换成 currentView 变量绑定的这个组件-->
+        <div :is="currentView" keep-alive></div>
+      </el-col>
+    </el-row>
   </div>
 </template>
-
 <script>
-  export default {
-    data () {
-      return {
-        userEntity: '',
-        dataForm: {
-          key: ''
-        },
-        dataList: [],
-        pageIndex: 1,
-        pageSize: 10,
-        totalPage: 0,
-        dataListLoading: false,
-        dataListSelections: [],
-        addOrUpdateVisible: false
-      }
-    },
-    components: {
-    },
-    activated () {
-      this.getDataList()
-      var tabSession = sessionStorage.getItem('tabSession')
-      this.userEntity = JSON.parse(tabSession)
-      console.log(this.userEntity)
-    },
-    methods: {
-      // 获取数据列表
-      getDataList () {
-        this.dataListLoading = true
-        this.$http({
-          url: this.$http.adornUrl('/operlogdetails/list'),
-          method: 'get',
-          params: this.$http.adornParams({
-            'page': this.pageIndex,
-            'limit': this.pageSize,
-            'key': this.dataForm.key
-          })
-        }).then(({data}) => {
-          if (data && data.code === 0) {
-            this.dataList = data.page.list
-            this.totalPage = data.page.totalCount
-          } else {
-            this.dataList = []
-            this.totalPage = 0
-          }
-          this.dataListLoading = false
-        })
-      },
-      // 每页数
-      sizeChangeHandle (val) {
-        this.pageSize = val
-        this.pageIndex = 1
-        this.getDataList()
-      },
-      // 当前页
-      currentChangeHandle (val) {
-        this.pageIndex = val
-        this.getDataList()
-      },
-      // 多选
-      selectionChangeHandle (val) {
-        this.dataListSelections = val
-      }
+import menu from '../menu.json'
+export default {
+  components: {
+    // 以下方式引入路由是路由的懒加载，有利于页面优化
+    businessCard: resolve => {
+      require(['./businessCard.vue'], resolve)
     }
+  },
+  data () {
+    return {
+      menu: [], // 菜单
+      tabPosition: 'left',
+      currentView: 'businessCard' // 当前组件
+    }
+  },
+  methods: {},
+  created () {
+    this.menu = menu
   }
+}
 </script>
+<style scoped>
+*{
+  margin: 0;
+  padding: 0;
+}
+.head{
+  margin-bottom: 20px;
+}
+.head-box {
+  background: rgb(223, 237, 243);
+  padding: 20px;
+  box-sizing: border-box;
+  border-radius: 5px;
+  display: flex;
+}
+.head-box div{
+  flex: 1;
+}
+.head-right{
+  display: flex;
+  padding-left: 20px;
+  padding-top: 0;
+  height: 100%;
+  box-sizing: border-box;
+}
+.head-left{
+  font-size: 24px;
+  border-right: 1px #97c2e8 solid;
+}
+.head-right dl{
+  flex: 1;
+  height: 100%;
+  margin: 0;
+}
+.head-right dl dd{
+  font-size: 16px;
+  color: #888;
+  display: flex;
+  align-items: center;
+  justify-content:center;
+}
+.head-right dl dt{
+  display: flex;
+  align-items: center;
+  justify-content:center;
+  margin-top: 10px;
+}
+.head-right dl dt p{
+  background: #b2d8f6;
+  color: #64a7e3;
+  display: inline;
+  padding: 3px 7px;
+  box-sizing: border-box;
+  font-size: 12px;
+}
+.head-right dl dt span{
+  color: white;
+  background: #85d266;
+  padding: 3px 7px;
+  box-sizing: border-box;
+  font-size: 12px;
+}
+</style>
